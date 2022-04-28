0a89105866f2f875f17c9b49d305eb348ce3aad1
diana.sevilla265@gmail.com <>
2022-04-28 18:43:01 -0500

    Prueba CES1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,17 @@
+
+package pruebaces1;
+
+/**
+ *
+ * @author usuario
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT HUB SEVILLA 2842022");
+    }
+    
+}
